20aea9537caee3e80cee2f4c423784eac1c462cf
teitumx <kirik241@ya.ru>
2020-03-26 23:28:54 +0300

    Добавлена домашняя работа  #5
    Вроде бы всё работает, но всё равно выдаёт ошибки, не понял из-за чего

---
+++ js2-lesson-5/js/main.js
@@ -0,0 +1,41 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
+const app = new Vue({
+  el: '#app',
+  data: {
+    catalogUrl: '/catalogData.json',
+    products: [],
+    searchLine: "",
+    isVisibleCart: true,
+    imgCatalog: 'https://placehold.it/200x150'
+  },
+
+
+  methods: {
+
+    getJson(url) {
+      return fetch(url)
+        .then(result => result.json())
+        .catch(error => {
+          console.log(error);
+        })
+    },
+
+    addProduct(product) {
+      console.log(product.id_product);
+    }
+  },
+
+  mounted() {
+    this.getJson(`${API + this.catalogUrl}`)
+      .then(data => {
+        for (let product of data) {
+          this.products.push(product);
+        }
+      });
+  },
+
+  filterGoods() {
+
+  }
+});
